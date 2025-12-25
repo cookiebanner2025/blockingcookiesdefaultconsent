ed7d0391ba0ed323dde138c7a49fc3de4654ef1b
cookiebanner2025 <analyticsmaster2@gmail.com>
2025-12-25 22:36:39 +0600

    Update blockingcookiesdefaultconsent.js

--- blockingcookiesdefaultconsent.js
+++ blockingcookiesdefaultconsent.js
@@ -3401,7 +3401,7 @@ function injectConsentHTML(detectedCookies, language = 'en') {
     /* When restricting clicks, make overlay block clicks */
     .cookie-blur-overlay.block-clicks {
         pointer-events: auto;
-        cursor: default;  <-- CHANGED TO "default"
+        cursor: default; 
     }
     
     /* When preventing scroll */
